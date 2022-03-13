56c9dad191150ff48b1b3025114e421c3a086a80
abhisatria <45398198+abhisatria@users.noreply.github.com>
2022-03-13 17:06:22 +0700

    comment PagedListUtil.kt

--- app/src/test/java/com/abhiwisesa/moviecatalogue/utils/PagedListUtil.kt
+++ app/src/test/java/com/abhiwisesa/moviecatalogue/utils/PagedListUtil.kt
@@ -6,15 +6,15 @@ import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
 
 object PagedListUtil {
-
-    fun <T> mockPagedList(list: List<T>): PagedList<T> {
-        val pagedList = mock(PagedList::class.java) as PagedList<T>
-        `when`(pagedList[anyInt()]).then { invocation ->
-            val index = invocation.arguments.first() as Int
-            list[index]
-        }
-        `when`(pagedList.size).thenReturn(list.size)
-
-        return pagedList
-    }
+//
+//    fun <T> mockPagedList(list: List<T>): PagedList<T> {
+//        val pagedList = mock(PagedList::class.java) as PagedList<T>
+//        `when`(pagedList[anyInt()]).then { invocation ->
+//            val index = invocation.arguments.first() as Int
+//            list[index]
+//        }
+//        `when`(pagedList.size).thenReturn(list.size)
+//
+//        return pagedList
+//    }
 }
